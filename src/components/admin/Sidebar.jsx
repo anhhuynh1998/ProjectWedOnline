78556680c24f1fb1d556df78603639b458b07129
anhhuynh1998 <anhprono1998@gmail.com>
2023-11-06 08:58:11 +0700

    nhatanh

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import { SiGoogleanalytics } from "react-icons/si";
-import { BiNews, BiRocket } from "react-icons/bi";
-import { FaWallet } from "react-icons/fa";
-import { AiFillSetting, AiFillAppstore } from "react-icons/ai";
-import { BsCashStack } from "react-icons/bs";
-import { MdAccountCircle } from "react-icons/md";
 import { NavLink } from "react-router-dom";
-
 export default function Sidebar() {
 
 
