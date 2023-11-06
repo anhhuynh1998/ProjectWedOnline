c9c060d39f029b6102d6238b3db9fad4b597a166
anhhuynh1998 <anhprono1998@gmail.com>
2023-11-06 08:06:59 +0700

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,62 +5,46 @@ import { FaWallet } from "react-icons/fa";
 import { AiFillSetting, AiFillAppstore } from "react-icons/ai";
 import { BsCashStack } from "react-icons/bs";
 import { MdAccountCircle } from "react-icons/md";
+import { NavLink } from "react-router-dom";
+
 export default function Sidebar() {
-  const links = [
-    {
-      title: "Dashboard",
-      icon: SiGoogleanalytics,
-    },
-    {
-      title: "Buy / Sell",
-      icon: AiFillAppstore,
-    },
-    {
-      title: "News",
-      icon: BiNews,
-    },
 
-    {
-      title: "Send Money",
-      icon: BiRocket,
-    },
-    {
-      title: "Deposit",
-      icon: BsCashStack,
-    },
 
-    {
-      title: "Transactions",
-      icon: FaWallet,
-    },
-    {
-      title: "Account",
-      icon: MdAccountCircle,
-    },
-    {
-      title: "Settings",
-      icon: AiFillSetting,
-    },
-  ];
+
   return (
     <div className="sidebar">
-      <div className="brand">
+      <div className="">
         <h2>
           FIN<span>CHECK</span>
         </h2>
       </div>
       <ul className="links">
-        {links.map((link) => {
-          return (
-            <li>
-              <a href="#">
-                {<link.icon />}
-                {link.title}
-              </a>
-            </li>
-          );
-        })}
+        <div>
+          <li>
+            <NavLink to={`/`}>
+              <i className="fa-solid fa-house"></i>
+              DASHBOARD
+            </NavLink>
+          </li>
+        </div>
+        <div>
+          <li>
+            <NavLink to={`/products`}>
+              <i className="fa-solid fa-list"></i>
+              PRODUCTS
+            </NavLink>
+          </li>
+        </div>
+        <div>
+          <li>
+            <NavLink to={`/users`}>
+              <i className="fa-solid fa-user"></i>
+              USERS
+            </NavLink>
+          </li>
+        </div>
       </ul>
     </div>
   );
+
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
-import './scss/styles.scss'
+import '../src/cssAdmin/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
