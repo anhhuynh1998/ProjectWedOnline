d3a87cba3088b703bf77b053b11d3ab1015e5eeb
QuocPhap696 <Dquocphap@gmail.com>
2023-12-19 09:08:59 +0700

    phap 19-12 làm thay đổi status

---
+++ src/components/admin/cart/AddProduct.jsx
@@ -0,0 +1,19 @@
+import AddProductMan from "./AddProductMan";
+import AddProductWomen from "./AddProductWomen";
+
+
+const AddProduct = () => {
+    return (
+        <>
+            <div>
+                <AddProductMan />
+            </div>
+            <div>
+                <AddProductWomen />
+            </div>
+        </>
+
+    );
+};
+
+export default AddProduct;
